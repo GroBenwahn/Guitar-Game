ed01eb0aa232d40ccabb387c56cdcabf0838a125
Hyeon818 <142474373+Hyeon818@users.noreply.github.com>
2024-08-10 23:04:14 +0900

    Debug fetch (08-10)

--- guitar rhythm game/Assets/Scripts/Update User.cs
+++ guitar rhythm game/Assets/Scripts/Update User.cs
@@ -14,7 +14,8 @@ public class UpdateUser : MonoBehaviour
     public InputField editpasswordInputField; // 여기에 이름 수정
 
     public void OnProfileBox()
-    {
+    {   
+        // InputField 초기화
         editnameInputField.text = "";
         editemailInputField.text = "";
         editpasswordInputField.text = "";
@@ -22,47 +23,6 @@ public class UpdateUser : MonoBehaviour
         editnameInputField.text = PlayerPrefs.GetString("name");
         editemailInputField.text = PlayerPrefs.GetString("email");
     }
-
-    /*
-    // 사용자 정보 요청 코루틴
-    private IEnumerator RequestUserInfo(string name, string email)
-    {
-        string url = baseURL + "get_user.php";
-
-        WWWForm form = new WWWForm();
-        form.AddField("name", name);
-        form.AddField("email", email);
-
-        using (UnityWebRequest request = UnityWebRequest.Post(url, form))
-        {
-            yield return request.SendWebRequest();
-
-            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
-            {
-                Debug.LogError("Error: " + request.error);
-            }
-            else
-            {
-                string json = request.downloadHandler.text;
-                Debug.Log("Response: " + json);
-
-                // JSON 파싱
-                Response response = JsonUtility.FromJson<Response>(json);
-
-                if (response.success)
-                {
-                    Debug.Log("Get User Information successfully." + response.name + "!");
-                    editnameInputField.text = name;
-                    editemailInputField.text = email;
-                }
-                else
-                {
-                    Debug.LogError("Failed to Login: " + response.message);\
-                }
-            }
-        }
-    }
-    */
 
     // 사용자 추가 메소드
     public void OnEditUserButtonClick()
@@ -86,12 +46,14 @@ public class UpdateUser : MonoBehaviour
         string url = baseURL + "edit_user.php";
 
         WWWForm form = new WWWForm();
+        // 이전 정보
         form.AddField("old_name", old_name);
-        form.AddField("old_email", old_email); // 여기에서 'email'을 'old_email'로 수정
+        form.AddField("old_email", old_email);
 
-        form.AddField("new_name", new_name); // 여기에서 'name'을 'new_name'으로 수정
-        form.AddField("new_email", new_email); // 여기에서 'email'을 'new_email'으로 수정
-        form.AddField("new_password", new_password); // 여기에서 'password'를 'new_password'로 수정
+        // 변경된 정보
+        form.AddField("new_name", new_name);
+        form.AddField("new_email", new_email);
+        form.AddField("new_password", new_password);
 
         using (UnityWebRequest request = UnityWebRequest.Post(url, form))
         {
